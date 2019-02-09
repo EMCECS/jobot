b3345ea9b5755d96ff4b8af2c60bcf4149dcfb0e
Oleg Ignatenko <oignatenko@gridgain.com>
2019-02-09 23:29:11 +0300

    cleanup some scaffolding stuff - verified with overview of diffs with master and rebuild

--- src/main/java/com/dell/jobot/Main.java
+++ src/main/java/com/dell/jobot/Main.java
@@ -12,8 +12,8 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Main {
 
-	private static final int QUEUE_CAPACITY = 10; // todo recover larger value after smoke testing: 1_000_000;
-	private static final int CACHE_CAPACITY = 10; // todo recover larger value after smoke testing: 1_000_000;
+	static final int QUEUE_CAPACITY = 1_000_000;
+	static final int CACHE_CAPACITY = 1_000_000;
 
 	public static void main(final String... args) {
 		if(0 == args.length) {
@@ -24,15 +24,10 @@ public class Main {
 		HttpUrlStreamHandler handler = setupHandler(executor);
 		try {
 			handler.handle(null, Arrays.stream(args));
-			executor.awaitTermination(/*todo recover larger value after smoke testing: Long.MAX_VALUE*/ 5, SECONDS);
+			executor.awaitTermination(Long.MAX_VALUE, SECONDS);
 		} catch(final Exception e) {
 			e.printStackTrace(System.err);
 		}
-	}
-
-	private static HttpUrlStreamHandler setupHandler(ThreadPoolExecutor executor) {
-		val uniqueUrlFilter = new FixedCacheUniquenessFilter<URL>(CACHE_CAPACITY);
-		return new HttpUrlStreamHandler(executor, url -> HTTP_FILTER.test(url) && uniqueUrlFilter.test(url));
 	}
 
 	static ThreadPoolExecutor setupExecutor() {
@@ -43,6 +38,11 @@ public class Main {
 		return new ThreadPoolExecutor(
 				parallelism, parallelism, 0, SECONDS, queue, threadFactory, rejectionHandler
 		);
+	}
+
+	private static HttpUrlStreamHandler setupHandler(ThreadPoolExecutor executor) {
+		val uniqueUrlFilter = new FixedCacheUniquenessFilter<URL>(CACHE_CAPACITY);
+		return new HttpUrlStreamHandler(executor, url -> HTTP_FILTER.test(url) && uniqueUrlFilter.test(url));
 	}
 
 	private static void printUsage() {
